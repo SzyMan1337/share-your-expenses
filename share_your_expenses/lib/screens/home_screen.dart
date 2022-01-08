f0e70554a4f03cd13992d78d6386a32117998390
MateuszSzymanski <szymanski.mateusz133@gmail.com>
2022-01-08 13:26:21 +0100

    Adding home screen and assets

---
+++ share_your_expenses/lib/screens/home_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:share_your_expenses/shared/common_button.dart';
+
+class HomeScreen extends StatefulWidget {
+  static String routeName = 'homeScreen';
+
+  const HomeScreen({Key? key}) : super(key: key);
+  static Route<HomeScreen> route() {
+    return MaterialPageRoute<HomeScreen>(
+      settings: RouteSettings(name: routeName),
+      builder: (BuildContext context) => const HomeScreen(),
+    );
+  }
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final loginScaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Image.asset(
+              "assets/images/logo.png",
+              height: 100,
+              width: 100,
+            ),
+            Image.asset(
+              "assets/images/friends.jpg",
+              height: MediaQuery.of(context).size.height / 3,
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.fitWidth,
+            ),
+            Text(
+              "Share your expenses now!",
+              style: Theme.of(context).textTheme.headline2,
+              textAlign: TextAlign.center,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CommonButton(
+                  onPressed: () {
+                    // CoffeeRouter.instance.push(
+                    //   LoginScreen.route(loginScaffoldKey),
+                    // );
+                  },
+                  text: 'Register',
+                  highlighColor: true,
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                CommonButton(
+                  onPressed: () {
+                    // Navigator.of(context).push(
+                    //   LoginScreen.route(loginScaffoldKey),
+                    // );
+                  },
+                  text: 'Log In',
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ share_your_expenses/lib/shared/const.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color darkBrown = Color(0xFFA26E47);
+const Color lightBrown = Color(0xFFF9E8D4);
+const Color brown = Color(0xFF9C5700);
+const Color facebookColor = Color(0xFF4867AA);
