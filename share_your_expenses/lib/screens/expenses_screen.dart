31fc1d24a1f6f2468e7ba3c1c2a99181a7bd7d3e
MateuszSzymanski <szymanski.mateusz133@gmail.com>
2022-01-10 23:07:29 +0100

    Adding expenses screen

---
+++ share_your_expenses/lib/screens/expenses_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ExpensesScreen extends StatefulWidget {
+  const ExpensesScreen({Key? key}) : super(key: key);
+
+  @override
+  _ExpensesScreenState createState() => _ExpensesScreenState();
+}
+
+class _ExpensesScreenState extends State<ExpensesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const BackButton(color: Colors.white),
+        centerTitle: true,
+        title: const Text("Flat Expenses"),
+        // bottom: const PreferredSize(
+        //     preferredSize: Size.fromHeight(1),
+        //     child: Text("Mateusz, Ola, Jacek")),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Image.asset(
+              "assets/images/logo.png",
+              semanticLabel: 'logo',
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
